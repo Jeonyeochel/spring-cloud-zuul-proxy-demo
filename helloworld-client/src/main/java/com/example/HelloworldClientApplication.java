679fe2a29b046915254e99b2b26d8f2d5e3e03ec
Younjin Jeong <younjin.jeong@gmail.com>
2016-09-22 18:05:01 +0900

    adding zuul proxy filters

--- helloworld-client/src/main/java/com/example/HelloworldClientApplication.java
+++ helloworld-client/src/main/java/com/example/HelloworldClientApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import com.example.filters.pre.SimpleFilter;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,5 +14,10 @@ public class HelloworldClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldClientApplication.class, args);
+	}
+
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
 	}
 }
